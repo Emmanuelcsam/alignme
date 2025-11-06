3dba835e18d098034ab9e31e08c4fbe4482d8a61
Emmanuel Sampson <ecsampson03@gmail.com>
2025-11-06 16:44:05 -0500

    photodetector code for wheel one if the wheel is sensed by the detector it will print buddha detected

--- photodetector.py
+++ photodetector.py
@@ -2,3 +2,15 @@ import RPi.GPIO as GPIO
 
 PHOTO1_PIN = 24  # For W1
 PHOTO2_PIN = 25  # For W2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PHOTO1_PIN, GPIO.IN)
+GPIO.setup(PHOTO2_PIN, GPIO.IN)
+
+sensor1 = GPIO.input(PHOTO1_PIN)
+sensor2 = GPIO.input(PHOTO2_PIN)
+
+if sensor1:
+    print("W1 Buddha detected")
+else:
+    print("W1 Buddha not detected")
